9bb2b454f80d15ef0cffa461f9df2228585c2e18
128bitpotato <skickligt01.flagig@icloud.com>
2025-03-17 17:45:38 +0100

    setting up html objects

---
+++ src/html_content.py
@@ -0,0 +1,45 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def get_html(url):
+        html = requests.get(url)
+        soup = BeautifulSoup(html.text, "html.parser")
+        return HTMLFile(soup)
+
+class HTMLContent():
+     def __init__(self, type, html):
+          self.type = type
+          self.html = html
+
+class HTMLFile():
+    def __init__(self, html):
+        self._html = html
+        self.content = []
+        
+        self.extract_title()
+        self.extract_description()
+        self.extract_ingredients()
+        self.extract_instructions()
+        self.extract_image()
+
+    def extract_title(self):
+        title = self._html.find_all("h1", class_=re.compile("title"))
+        self.content.append(title)
+
+    def extract_description(self):
+        description = self._html.find_all("div", class_=re.compile("description"))
+        self.content.append(description)
+
+    def extract_ingredients(self):
+        ingredients = self._html.find_all("span", class_=re.compile("ingredient"))
+        self.content.append(ingredients)
+
+    def extract_instructions(self):
+        instructions = self._html.find_all("span", class_=re.compile("instruction"))
+        self.content.append(instructions)
+
+    def extract_image(self):
+        image = self._html.find_all("source", srcset=True)
+        self.content.append(image)
+    
