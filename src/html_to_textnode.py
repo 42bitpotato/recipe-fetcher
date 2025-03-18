711a5bf94eb064e62a6f34950933d843ae73f0a0
128bitpotato <skickligt01.flagig@icloud.com>
2025-03-18 18:24:38 +0100

    Added ingredients to textnode

--- src/html_to_textnode.py
+++ src/html_to_textnode.py
@@ -4,7 +4,6 @@ from textnode import *
 from bs4 import BeautifulSoup
 
 
-
 def html_to_textnode(html_file):
     if len(html_file.content) == 0:
         raise ValueError(f"Missing content: {html_file.content}")
@@ -16,4 +15,14 @@ def title_to_textnode(html_title):
 def description_to_textnode(html_description):
     string = str(html_description.p.string)
     return ContentChildNode(string.strip())
-    
+
+def ingredients_to_textnode(html):
+    list_of_ingredients = []
+    for ingredient in html:
+        ingredient_txt = str(ingredient.string)
+        for line in ingredient_txt.splitlines():
+            if line.isspace() or line == "":
+                continue
+            else:
+                list_of_ingredients.append(ContentChildNode(line.strip()))
+    return list_of_ingredients
