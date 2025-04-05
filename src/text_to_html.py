1422e30414356b96f29f731757d20b4c1cb2f358
128bitpotato <skickligt01.flagig@icloud.com>
2025-04-05 11:53:45 +0200

    Move away old code

--- src/text_to_html.py
+++ src/text_to_html.py
@@ -39,30 +39,3 @@ def remove_name_spaces(file_name):
     new_name = file_name.replace(" ", "_")
     new_name.join(".html")
     return new_name
-
-def copy_template_1(recipe_name, template_path=None, destination_path=None):
-    template_path = "html_template" if template_path == None else template_path
-    destination_path = "pdf_recipes" if destination_path == None else destination_path
-
-    html_template_path = os.path.join(template_dir, "template.html")
-    css_template = os.path.join(template_dir, "style.css")
-
-    if not os.path.exists(template_path):
-        raise ValueError(f'Template path not found: "{template_path}", please create directory if missing')
-    if not os.path.exists(destination_path):
-        raise ValueError(f'Destination path not found: "{destination_path}", please create directory if missing')
-
-    template_path_content = os.listdir(path=template_path)
-    destination_path_content = os.listdir(path=destination_path)
-
-    new_template_name = remove_name_spaces(recipe)
-    new_template_path = os.path.join(destination_path, new_template_name)
-
-    for i in template_path_content:
-        if i == "template.html":
-            path = os.path.join(template_path, i)
-            for j in destination_path_content:
-                if j == new_template_name:
-                    rm_path = os.path.join(destination_path, j)
-                    os.remove(rm_path)
-            shutil.copy(path, new_template_path)
