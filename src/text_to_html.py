4f701d7d35c04592119c0984bffebc3377720580
128bitpotato <skickligt01.flagig@icloud.com>
2025-04-06 12:00:18 +0200

    Progressing on text to html function

--- src/text_to_html.py
+++ src/text_to_html.py
@@ -19,6 +19,32 @@ def text_to_html(recipe_text: "RecipeHead"):
                 if line[c] == ">":
                     new_line = line[:c+1] + sections[SectionType.TITLE] + line[c+1:]
                     html_template_split[line_num] = new_line
+                    break
+        if strip_line.startswith("<h1>"):
+            line = html_template_split[line_num]
+            for c in range(len(line)):
+                if line[c] == ">":
+                    new_line = line[:c+1] + sections[SectionType.TITLE] + line[c+1:]
+                    html_template_split[line_num] = new_line
+                    break
+        
+        # Description
+        if strip_line.startswith('<p class="description">'):
+            line = html_template_split[line_num+1]
+            new_line = line + sections[SectionType.DESCR]
+            html_template_split[line_num+1] = new_line
+            break
+
+        # Ingredients
+        if strip_line.startswith("<h2>Ingredients</h2>"):
+            ing_line_num = line_num+2
+            line = html_template_split[ing_line_num]
+            
+            for l in range(len(html_template_split[ing_line_num+1])):
+                if line[c] == ">":
+                    new_line = line[:c+1] + sections[SectionType.TITLE] + line[c+1:]
+                    html_template_split[line_num] = new_line
+                    break
 
 def copy_template(template_dir=None):
     template_dir = "html_template" if template_dir == None else template_dir
@@ -33,8 +59,9 @@ def copy_template(template_dir=None):
     
     return html_template
 
-def add_text_to_line(html_template_split, line_num, enum: "SectionType"):
-    line = html_template_split[line_num]
+def add_text_to_line(line, start, end, enum: "SectionType"):
+    new_line = line[start] + sections[SectionType.TITLE] + line[end]
+    return new_line
     # If multiple lines, split list and add bottom to new list before extending with new lines, then add old lines again
 
 def remove_name_spaces(file_name):
