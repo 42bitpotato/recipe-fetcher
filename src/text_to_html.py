ba27ebeafba2f1e69c209ccc20004e1a7702ac96
128bitpotato <skickligt01.flagig@icloud.com>
2025-04-06 09:39:29 +0200

    Progressing on text to html function

--- src/text_to_html.py
+++ src/text_to_html.py
@@ -1,22 +1,24 @@
 import os
 import shutil
-from textnode import RecipeHead
+from textnode import RecipeHead, SectionType
 
 def text_to_html(recipe_text: "RecipeHead"):
     html_template = copy_template()
 
-    sections = section_dictionary(recipe_text)
+    sections = recipe_text.sections_dict
 
     html_template_split = html_template.splitlines()
     
-    for line_num in range(html_template_split):
+    for line_num in range(len(html_template_split)):
         strip_line = html_template_split[line_num].strip() # Remove leading and trailing whitespaces
 
         # Title
         if strip_line.startswith("<title>"):
-            for c in html_template_split[line_num]:
-                if c == ">":
-                    pass
+            line = html_template_split[line_num]
+            for c in range(len(line)):
+                if line[c] == ">":
+                    new_line = line[:c+1] + sections[SectionType.TITLE] + line[c+1:]
+                    html_template_split[line_num] = new_line
 
 def copy_template(template_dir=None):
     template_dir = "html_template" if template_dir == None else template_dir
@@ -31,9 +33,9 @@ def copy_template(template_dir=None):
     
     return html_template
 
-def section_dictionary(recipe_head):
-    pass
-
+def add_text_to_line(html_template_split, line_num, enum: "SectionType"):
+    line = html_template_split[line_num]
+    # If multiple lines, split list and add bottom to new list before extending with new lines, then add old lines again
 
 def remove_name_spaces(file_name):
     new_name = file_name.replace(" ", "_")
