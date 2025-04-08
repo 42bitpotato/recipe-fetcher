209c54c169c8e02e5b79b0fc694f175b89fc9958
128bitpotato <skickligt01.flagig@icloud.com>
2025-04-08 06:13:00 +0200

    Debugging

--- src/text_to_html.py
+++ src/text_to_html.py
@@ -3,12 +3,13 @@ from textnode import RecipeHead, SectionType
 
 def text_to_html(recipe_obj: "RecipeHead"):
     html_template = copy_template()
-
     sections = recipe_obj.sections
-
     html_template_split = html_template.splitlines()
     
-    for line_num in range(len(html_template_split)):
+    line_num = 0
+
+    while line_num < len(html_template_split) - 1:
+        line_num += 1
         strip_line = html_template_split[line_num].strip() # Remove leading and trailing whitespaces
 
         # Title
@@ -19,7 +20,7 @@ def text_to_html(recipe_obj: "RecipeHead"):
                     new_line = line[:c+1] + sections[SectionType.TITLE] + line[c+1:]
                     html_template_split[line_num] = new_line
                     break
-        if strip_line.startswith("<h1>"):
+        elif strip_line.startswith("<h1>"):
             line = html_template_split[line_num]
             for c in range(len(line)):
                 if line[c] == ">":
@@ -29,14 +30,14 @@ def text_to_html(recipe_obj: "RecipeHead"):
             continue
         
         # Description
-        if strip_line.startswith('<p class="description">'):
+        elif strip_line.startswith('<p class="description">'):
             line = html_template_split[line_num+1]
             new_line = line + sections[SectionType.DESCR]
             html_template_split[line_num+1] = new_line
             continue
 
         # Image
-        if strip_line.startswith("<img"):
+        elif strip_line.startswith("<img"):
             line = html_template_split[line_num]
             for c in range(len(line)):
                 if line[c] == '"':
@@ -46,32 +47,31 @@ def text_to_html(recipe_obj: "RecipeHead"):
             continue
 
         # Ingredients
-        if strip_line.startswith("<h2>Ingredients</h2>"):
+        elif strip_line.startswith("<h2>Ingredients</h2>"):
             ing_line_num = line_num+2
             line = html_template_split[ing_line_num]
 
-            spaces = " " * line.count(" ") # Count indentation spaces
-            list_items = sections[SectionType.INGRE].copy()
-
-            add_list_item(html_template_split, ing_line_num, spaces, list_items)
+            add_list_item(html_template_split, ing_line_num, sections[SectionType.INGRE])
             continue
 
-        # Instructions
-        if strip_line.startswith("<h2>Instructions</h2>"):
-            ing_line_num = line_num+2
-            line = html_template_split[ing_line_num]
+        # Instructions 
+        elif strip_line.startswith("<h2>Instructions</h2>"):
+            ins_line_num = line_num+2
+            line = html_template_split[ins_line_num]
 
-            spaces = " " * line.count(" ") # Count indentation spaces
-            list_items = sections[SectionType.INSTR].copy()
-
-            add_list_item(html_template_split, ing_line_num, spaces, list_items)
-            continue
+            add_list_item(html_template_split, ins_line_num, sections[SectionType.INSTR])
+            continue 
 
     return "\n".join(html_template_split)
 
-def add_list_item(html_template_split, line_nr, spaces, list_items):
-    if len(list_items) == 0:
-        return
-    line = spaces + "<li>" + list_items.pop() + "</li>"
-    html_template_split[line_nr:line_nr] = line
-    return add_list_item(html_template_split, line_nr + 1, spaces, list_items)
+def add_list_item(html_template_split, line_nr, list_items):
+    line = html_template_split[line_nr]
+    spaces = line[:len(line) - len(line.lstrip())] + "  " # Indent list in html
+
+    for i, item in enumerate(list_items):
+        item_line = spaces + f"<li>{item}</li>"
+        html_template_split[line_nr+i:line_nr+i] = [item_line]
+
+    if html_template_split[line_nr + len(list_items)].strip() == "":
+        html_template_split.pop(line_nr + len(list_items))
+
