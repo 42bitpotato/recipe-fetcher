2c226ddaabe3d8214c1e1dd906b08da7f9f9800c
128bitpotato <skickligt01.flagig@icloud.com>
2025-04-09 21:45:52 +0200

    copy css file function

--- src/file_handling.py
+++ src/file_handling.py
@@ -18,3 +18,16 @@ def remove_name_spaces(file_name):
     new_name = file_name.replace(" ", "_")
     new_name.join(".html")
     return new_name
+
+def copy_css(css_dir=None):
+    css_dir = "html_template" if css_dir == None else css_dir
+
+    css_template_path = os.path.join(css_dir, "style.css")
+
+    if os.path.exists(css_template_path):
+        with open(css_template_path, "r") as css_file:
+            css_template = css_file.read()
+    else:
+        raise ValueError(f'CSS template file not found: "{css_template_path}"')
+    
+    return css_template
